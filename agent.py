9ee54d2f1e8d4437a36d4a6d545e35085dea17d6
LOGIC LEGION <ommtripathy07@gmail.com>
2026-06-28 18:49:02 +0530

    Moving news tool

--- agent.py
+++ agent.py
@@ -1,6 +1,3 @@
-
-
-
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,7 +7,6 @@ import requests
 from langchain_mistralai import ChatMistralAI
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage, ToolMessage
-from tavily import TavilyClient
 from rich import print
 from langchain.agents import create_agent
 from langchain.agents.middleware import wrap_tool_call
@@ -21,41 +17,11 @@ from tools.weather import get_weather
 
 print(get_weather.invoke("Bhopal"))
 
-# ============================================================
-#                    📰 NEWS TOOL
-# ============================================================
+# Import the News Tool
+from tools.news import get_news
 
-tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+print(get_news.invoke("Bhopal"))
 
 
-@tool
-def get_news(city: str) -> str:
-    """Get latest news about the city."""
 
-    response = tavily_client.search(
-    query=f"Latest news about {city}",
-    topic="news",
-    search_depth="basic",
-    max_results=5
-)
-
-    results = response.get("results", [])
-
-    if not results:
-        return f"No news found for {city}"
-
-    news_list = []
-
-    for r in results:
-        title = r.get("title", "No title")
-        url = r.get("url", "")
-        snippet = r.get("content", "")
-
-        news_list.append(
-            f"- {title}\n"
-            f"{url}\n"
-            f"{snippet[:100]}..."
-        )
-
-    return f"Latest news for {city.title()}:\n\n" + "\n\n".join(news_list)
 
---
+++ tools/news.py
@@ -0,0 +1,47 @@
+# ============================================================
+#                    📰 NEWS TOOL
+# ============================================================
+
+import os
+
+from tavily import TavilyClient
+from langchain.tools import tool
+
+# Create a Tavily client using the API key from the .env file
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+@tool
+def get_news(city: str) -> str:
+    """Get latest news about the city."""
+
+    response = tavily_client.search(
+    query=f"Latest news about {city}",
+    topic="news",
+    search_depth="basic",
+    max_results=5
+    )
+
+    # Get the search results
+    results = response.get("results", [])
+
+    # If nothing is found
+    if not results:
+        return f"No news found for {city}."
+
+    news_list = []
+
+    # Loop through every news article
+    for article in results:
+
+        title = article.get("title", "No Title")
+        url = article.get("url", "")
+        snippet = article.get("content", "")
+
+        news_list.append(
+            f"- {title}\n"
+            f"{url}\n"
+            f"{snippet[:100]}..."
+        )
+
+    # Return all news articles
+    return f"Latest news for {city.title()}:\n\n" + "\n\n".join(news_list)
