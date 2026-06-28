08e59ad3fa46d392eef7b352cd3452fc5a0bf543
LOGIC LEGION <ommtripathy07@gmail.com>
2026-06-28 11:19:06 +0530

    Add news tool

--- Agents.py
+++ Agents.py
@@ -18,7 +18,7 @@ from tavily import TavilyClient
 # weather tool
 
 @tool 
-def weather(city : str) -> str :
+def get_weather(city : str) -> str :
     """ Get current weather in a city """
     api_key = os.getenv("OPENWEATHER_API_KEY")
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
@@ -36,4 +36,48 @@ def weather(city : str) -> str :
 
     return f"weather in {city.title()} is {temp}°C with {desc}. Humidity: {humidity}%. Wind: {wind_speed}km/h"
 
-print(weather.invoke("Bhopal"))
+print(get_weather.invoke("Bhopal"))
+
+# tavily news tool
+
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+
+@tool
+def get_news(city: str) -> str:
+    """Get latest news about the city."""
+
+    response = tavily_client.search(
+    query=f"Latest news about {city}",
+    topic="news",
+    search_depth="basic",
+    max_results=5
+)
+
+    results = response.get("results", [])
+
+    if not results:
+        return f"No news found for {city}"
+
+    news_list = []
+
+    for r in results:
+        title = r.get("title", "No title")
+        url = r.get("url", "")
+        snippet = r.get("content", "")
+
+        news_list.append(
+            f"- {title}\n"
+            f"{url}\n"
+            f"{snippet[:100]}..."
+        )
+
+    return f"Latest news for {city.title()}:\n\n" + "\n\n".join(news_list)
+
+print(get_news.invoke("Bhopal"))
+
+
+    
+    
+
+    
